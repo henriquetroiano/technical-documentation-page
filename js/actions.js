c5bfb2439607000e90d1821d96c04f61e623bed0
henriquetroiano <henriquetroiano@hotmail.com>
2020-07-21 18:58:26 -0300

    final version

--- js/actions.js
+++ js/actions.js
@@ -1,8 +1,9 @@
 const menuButton = document.querySelector('#navbar');
 
 menuButton.addEventListener('click', function(event ) {
+    window.scrollTo(0, 0)
     setTimeout(() => {
         menuButton.classList.toggle('hide');
         console.log(this.getBoundingClientRect())
-    }, 200)
+    }, 100)
 })
